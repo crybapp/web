615e9afc4ed4f57370424652fd640dfc6608ae2f
William Gibson <w.gibbo3@icloud.com>
2019-10-17 20:33:07 +0100

    Add support for no landing video
    Adding BRAND_LANDING_VIDEO_ID= to your .env file should now remove the landing video and resize other elements so everything looks ok

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="landing">
-        <div class="left">
+        <div :class="{ left: hasLandingVideo, 'has-landing-video': hasLandingVideo }">
             <div class="center">
                 <img src="/img/cryb-logo.svg" class="logo">
                 <h1 class="title">Share the internet with your friends</h1>
@@ -15,7 +15,7 @@
                 </div>
             </div>
         </div>
-        <div class="right">
+        <div class="right" v-if=hasLandingVideo>
             <iframe class="video" width="1920" height="1080" :src="`https://www.youtube.com/embed/${brand.landing_video_id}?controls=0&autoplay=1&loop=1&playlist=${brand.landing_video_id}`" frameborder="0" allow="accelerometer; autoplay; loop; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     </div>
@@ -34,7 +34,11 @@
             return { redirectUrl }
         },
         computed: {
-            ...mapGetters(['token'])
+            ...mapGetters(['token']),
+
+            hasLandingVideo() {
+                return this.brand.landing_video_id && this.brand.landing_video_id.length > 0
+            }
         },
         layout: 'logged-out',
         middleware: 'logged-out',
