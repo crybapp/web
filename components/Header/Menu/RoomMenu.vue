be581c770a30f08d92b630fb7ffe80ba83615505
JeDaYoshi <jedayoshi@disroot.org>
2019-11-19 06:04:16 -0400

    Remove automatic menu toggle when copying invite link
    This caused it to be toggled even if you closed it yourself - and I feel it should match the other buttons in action.

--- components/Header/Menu/RoomMenu.vue
+++ components/Header/Menu/RoomMenu.vue
@@ -98,10 +98,7 @@
                     await this.$copyText(`${process.env.BASE_WEB_URL}/i/${this.room.invites[0].code}`)
 
                     this.hint = 'Copied!'
-                    setTimeout(() => {
-                        this.$refs.menu.toggleMenu()
-                        this.hint = 'Click to copy link'
-                    }, 500)
+                    setTimeout(() => this.hint = 'Click to copy link', 500)
                 } catch(error) {
                     alert(error)
                 }
