e0997f81934b86bf08673aaaed6cef48c9f3a8c3
William Gibson <w.gibbo3@icloud.com>
2019-11-23 16:47:46 +0000

    Remove comment

--- brand/config.js
+++ brand/config.js
@@ -1,7 +1,5 @@
 /**
  * Note that you'll still need to change the logo, which is located under static/img.
- * We recommend that you keep the cryb_ prefix for these files to avoid any conflicts.
- * The cryb_ prefix won't be visible to users, and we'll remove any cryb- or cryb_ prefixes in static files in the future.
  */
 
 export default {
