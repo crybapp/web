5a82045bc44b08a6c3f414faef539478ac18177b
William Gibson <w.gibbo3@icloud.com>
2020-01-04 19:44:29 +0000

    Add pages support

--- components/Footer/index.vue
+++ components/Footer/index.vue
@@ -2,16 +2,21 @@
     <div class="footer">
         <p class="footer">
             &copy; {{ new Date().getFullYear() }} {{ brand.name }}
+			<span v-for="page in pages" :key=page.name>
+				&mdash; <a :href=page.link target="_blank">{{ page.name }}</a>
+			</span>
         </p>
     </div>
 </template>
 <script>
 	import brand from '~/brand/config'
+	import pages from '~/brand/pages'
 
 	export default {
 		data() {
 			return {
-				brand
+				brand,
+				pages
 			}
 		}
 	}
