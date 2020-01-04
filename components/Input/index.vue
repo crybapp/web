0da8fa393c78f22c2e3ef6be07a55648b76e6626
William Gibson <w.gibbo3@icloud.com>
2020-01-04 20:15:22 +0000

    Small updates

--- components/Input/index.vue
+++ components/Input/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <input ref="input" class="input" type="text" :value=value :placeholder=placeholder @input=didUpdateInput() @keydown.enter=didPressEnter />
+    <input ref="input" class="input" :class="{ 'is-loading': loading, 'is-disabled': disabled }" type="text" :value=value :placeholder=placeholder @input=didUpdateInput() @keydown.enter=didPressEnter />
 </template>
 <script>
     export default {
